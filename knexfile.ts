be996e0890946f99b165510bd8f331c4635edae8
Vagner Vengue <vagnervengue@users.noreply.github.com>
2021-04-15 00:00:32 -0300

    Heroku update.

--- knexfile.ts
+++ knexfile.ts
@@ -8,6 +8,7 @@ console.log('# Knexfile triggered. #');
 module.exports = {
     client: 'pg',
     connection: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
     migrations: {
         directory: path.resolve(__dirname, 'src', 'database', 'migrations')
     },
